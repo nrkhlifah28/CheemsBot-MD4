ab9ad42733c58c8c8eae7b5a088e1ca016c3c308
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-09-15 08:53:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,23 +55,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6289618728196'] //ur owner number
+global.ownername = "NoerKhlifah"
+global.ytname = "YT: -" //ur yt chanel name
+global.socialm = "GitHub: nrkhlifah28" //ur github or insta name
+global.location= "Indonesia" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['6289618728196']
+global.ownertag = '6289618728196' //ur tag number
+global.botname = '© Nrkhlifah' //ur bot name
 global.linkz = "https://youtu.be/xn9RatOrbuI" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
+global.reactmoji = "🗿" //ur menu react emoji
+global.themeemoji = "🗿" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\nGottcha bitch " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "NoerKhlifah" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -80,7 +80,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['628961872816'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
